a7b5b35051d853ee7f45737a8f6e059abbe65465
Boris <bwa@adorsys.de>
2014-04-30 21:14:42 +0100

    Create, update, delete and list productions tasks.

--- gpao-tatou/src/main/java/cm/adorsys/gpao/model/ProductionTaskConfig.java
+++ gpao-tatou/src/main/java/cm/adorsys/gpao/model/ProductionTaskConfig.java
@@ -2,8 +2,11 @@ package cm.adorsys.gpao.model;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
+
 import javax.validation.constraints.NotNull;
+import javax.persistence.EntityManager;
 import javax.persistence.ManyToOne;
+import javax.persistence.TypedQuery;
 
 @RooJavaBean
 @RooToString
@@ -29,4 +32,18 @@ public class ProductionTaskConfig extends GpaoBaseEntity {
      * The estimated task's duration in hours
      */
     private int duration;
+    
+    public static TypedQuery<cm.adorsys.gpao.model.ProductionTaskConfig> findProductionTaskConfigByIdUpperThan(Long id) {
+        EntityManager em = ProductionTaskConfig.entityManager();
+        TypedQuery<ProductionTaskConfig> q = em.createQuery("SELECT o FROM ProductionTaskConfig AS o WHERE  o.id > :id ORDER BY o.id ", ProductionTaskConfig.class);
+        q.setParameter("id", id);
+        return q;
+    }
+
+    public static TypedQuery<ProductionTaskConfig> findProductionTaskConfigByIdLowerThan(Long id) {
+        EntityManager em = ProductionTaskConfig.entityManager();
+        TypedQuery<ProductionTaskConfig> q = em.createQuery("SELECT o FROM ProductionTaskConfig AS o WHERE  o.id < :id ORDER BY o.id ", ProductionTaskConfig.class);
+        q.setParameter("id", id);
+        return q;
+    }
 }
--- gpao-tatou/src/main/java/cm/adorsys/gpao/web/ProductionTaskConfigController.java
+++ gpao-tatou/src/main/java/cm/adorsys/gpao/web/ProductionTaskConfigController.java
@@ -1,11 +1,87 @@
 package cm.adorsys.gpao.web;
-import cm.adorsys.gpao.model.ProductionTaskConfig;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cm.adorsys.gpao.model.CustomerOrder;
+import cm.adorsys.gpao.model.ProductionTaskConfig;
+import cm.adorsys.gpao.utils.MessageType;
 
 @RequestMapping("/productiontaskconfigs")
 @Controller
 @RooWebScaffold(path = "productiontaskconfigs", formBackingObject = ProductionTaskConfig.class)
 public class ProductionTaskConfigController {
+
+    @RequestMapping(value = "/addOrEditForm", method = RequestMethod.GET)
+    public String addOrEditProductionTaskConfigConfigForm(@RequestParam(value = "id", required = false) Long id, HttpServletRequest httpServletRequest, Model uiModel) {
+    	ProductionTaskConfig productionTaskConfig = null ;
+        if (id == null) {
+        	productionTaskConfig = new ProductionTaskConfig();
+        } else {
+        	productionTaskConfig = ProductionTaskConfig.findProductionTaskConfig(id);
+        }
+        populateEditForm(uiModel, productionTaskConfig);
+        return "productiontaskconfigs/productiontaskconfigsView";
+    }
+
+    @RequestMapping(value = "/addOrEdit", method = RequestMethod.POST)
+    public String addOrEditProductionTaskConfig(@Valid ProductionTaskConfig productionTaskConfig, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+        if (bindingResult.hasErrors()) {
+            populateEditForm(uiModel, productionTaskConfig);
+            uiModel.addAttribute(MessageType.ERROR_MESSAGE, "une erreur est Survenue durant l'enregistrement ! \n " + bindingResult.getFieldErrors());
+            return "productiontaskconfigs/productiontaskconfigsView";
+        }
+        if (productionTaskConfig.getId() == null) {
+        	productionTaskConfig.persist();
+        }
+        productionTaskConfig = doAConsistantMerge(productionTaskConfig);
+        populateEditForm(uiModel, productionTaskConfig);
+        uiModel.addAttribute(MessageType.SUCCESS_MESSAGE, "Enregistre avec success !");
+        return "productiontaskconfigs/productiontaskconfigsView";
+    }
+
+
+    @RequestMapping(value = "/next/{id}", method = RequestMethod.GET, produces = "text/html")
+    public String getNextProductionTask(@PathVariable("id") Long id, Model uiModel) {
+    	List<ProductionTaskConfig> productionTaskConfigs = ProductionTaskConfig.findProductionTaskConfigByIdUpperThan(id).setMaxResults(1).getResultList();
+        if (productionTaskConfigs.isEmpty()) {
+            populateEditForm(uiModel, ProductionTaskConfig.findProductionTaskConfig(id));
+            uiModel.addAttribute(MessageType.ERROR_MESSAGE, "Aucune configuration trouve !");
+            return "productiontaskconfigs/productiontaskconfigsView";
+        }
+        populateEditForm(uiModel, productionTaskConfigs.iterator().next());
+        return "productiontaskconfigs/productiontaskconfigsView";
+    }
+
+    @RequestMapping(value = "/previous/{id}", method = RequestMethod.GET, produces = "text/html")
+    public String getPreviousProductionTask(@PathVariable("id") Long id, Model uiModel) {
+        List<ProductionTaskConfig> productionTaskConfigs = ProductionTaskConfig.findProductionTaskConfigByIdLowerThan(id).setMaxResults(1).getResultList();
+        
+        if (productionTaskConfigs.isEmpty()) {
+            populateEditForm(uiModel, ProductionTaskConfig.findProductionTaskConfig(id));
+            uiModel.addAttribute(MessageType.ERROR_MESSAGE, "Aucune configuration trouve !");
+            return "productiontaskconfigs/productiontaskconfigsView";
+        }
+        populateEditForm(uiModel, productionTaskConfigs.iterator().next());
+        return "productiontaskconfigs/productiontaskconfigsView";
+    }
+    private ProductionTaskConfig doAConsistantMerge(ProductionTaskConfig productionTaskConfig) {
+        try {
+        	productionTaskConfig.merge();
+        } catch (Exception e) {
+        	productionTaskConfig.setVersion(CustomerOrder.findCustomerOrder(productionTaskConfig.getId()).getVersion());
+        	productionTaskConfig = productionTaskConfig.merge();
+        }
+        return productionTaskConfig;
+    }
 }
